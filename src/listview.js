d3653065ed3001a415e3ff5fa28564aebab34f17
cambell-prince <cambell.prince@gmail.com>
2014-04-02 15:07:10 +1300

    Update to angular 1.2.x and angular-ui 0.8.x

--- src/listview.js
+++ src/listview.js
@@ -6,7 +6,7 @@ angular.module('palaso.ui.listview', ['ui.bootstrap'])
 			restrict : 'EA',
 			transclude : true,
 			replace : true,
-			template : '<div class="listview" ng-hide="hideIfEmpty && items.length == 0"><div ng-transclude></div><div class="paginationblock"><pagination boundary-links="true" num-pages="noOfPages" current-page="currentPage" previous-text="\'&lsaquo;\'" next-text="\'&rsaquo;\'" first-text="\'&laquo;\'" last-text="\'&raquo;\'"></pagination><div class="pull-right pagination">Items per page: <select ng-model="itemsPerPage"><option value="10" selected>10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></div></div></div>',
+			template : '<div class="listview" ng-hide="hideIfEmpty && items.length == 0"><div ng-transclude></div><div class="paginationblock"><pagination boundary-links="true" total-items="itemsCount" items-per-page="itemsPerPage" page="currentPage" previous-text="&lsaquo;" next-text="&rsaquo;" first-text="&laquo;" last-text="&raquo;"></pagination><div class="right pagination">Items per page: <select ng-model="itemsPerPage"><option value="10" selected>10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></div></div></div>',
 			scope : {
 				search : "&",
 				select : "&",
@@ -16,6 +16,7 @@ angular.module('palaso.ui.listview', ['ui.bootstrap'])
 			},
 			controller: ["$scope", function($scope) {
 				$scope.itemsPerPage = 10;  // This should match the default value for the selector above
+				$scope.itemsCount = 0;
 				
 				this.activate = function(item) {
 					$scope.active = item;
@@ -49,20 +50,16 @@ angular.module('palaso.ui.listview', ['ui.bootstrap'])
 						sliceEnd = $scope.itemsPerPage;
 					}
 					$scope.visibleItems = $scope.items.slice(sliceStart, sliceEnd);
-				}
-				this.updatePages = function() {
-					$scope.noOfPages = Math.ceil($scope.items.length / $scope.itemsPerPage);
-					if ($scope.currentPage > $scope.noOfPages) {
-						// This can happen if items have been deleted, for example
-						$scope.currentPage = $scope.noOfPages;
-					}
+				};
+				this.updateItemsCount = function() {
+					$scope.itemsCount = $scope.items.length;
 					if ($scope.currentPage < 1) {
 						$scope.currentPage = 1;
 					}
-				}
+				};
 				this.query = function() {
 					$scope.search();
-					this.updatePages();
+					this.updateItemsCount();
 //					$scope.search({
 //						term : $scope.term
 //					});
@@ -73,13 +70,13 @@ angular.module('palaso.ui.listview', ['ui.bootstrap'])
 					controller.updateVisibleItems();
 				});
 				scope.$watch('itemsPerPage', function() {
-					controller.updatePages();
+					controller.updateItemsCount();
 					controller.updateVisibleItems();
 				});
 				scope.$watch('items', function() {
-					controller.updatePages();
+					controller.updateItemsCount();
 					controller.updateVisibleItems();
-				}, true)
+				}, true);
 				controller.query();
 			}
 		};
